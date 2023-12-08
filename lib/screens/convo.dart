1ea0b392fee9415f144606fcc7424450642b8d5b
Justin Bolivar <justinantonio.bolivar.21@usjr.edu.ph>
2023-12-08 15:00:58 +0800

    added robot asset and button

--- lib/screens/convo.dart
+++ lib/screens/convo.dart
@@ -1,12 +1,45 @@
 // back button goes to homescreen
 
 import 'package:flutter/material.dart';
+import 'package:gigglygizmo/screens/homescreen.dart';
 
 class Conversation extends StatelessWidget {
   const Conversation({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Image.asset(
+            'assets/Background.png',
+            fit: BoxFit.cover,
+            height: double.infinity,
+            width: double.infinity,
+            alignment: Alignment.center,
+          ),
+          Stack(
+            children: [
+              Positioned(
+                top: 55,
+                left: 30,
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                          builder: (context) => const HomeScreen()),
+                    );
+                  },
+                  child: Image.asset(
+                    'assets/arrowleft.png',
+                    alignment: Alignment.topRight,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
