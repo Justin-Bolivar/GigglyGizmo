de86194a463e6f45c4c2bafdc722776f14366663
Justin Bolivar <justinantonio.bolivar.21@usjr.edu.ph>
2023-12-08 15:52:45 +0800

    conversation page finished

--- lib/screens/convo.dart
+++ lib/screens/convo.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:gigglygizmo/screens/homescreen.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Conversation extends StatelessWidget {
   const Conversation({super.key});
@@ -17,6 +18,33 @@ class Conversation extends StatelessWidget {
             height: double.infinity,
             width: double.infinity,
             alignment: Alignment.center,
+          ),
+          Stack(
+            //message field temporary placeholder before real textfield
+            children: [
+              Positioned(
+                bottom: 10,
+                left: 40,
+                child: Image.asset('assets/border.png'),
+              ),
+              Positioned(
+                bottom: 30,
+                right: 55,
+                child: Image.asset('assets/mic.png'),
+              ),
+              Positioned(
+                bottom: 30,
+                left: 60,
+                child: Text(
+                  'type message here.....',
+                  style: GoogleFonts.chivo(
+                    color: Colors.grey,
+                    fontWeight: FontWeight.w200,
+                    fontSize: 15,
+                  ),
+                ),
+              ),
+            ],
           ),
           Stack(
             children: [
@@ -35,6 +63,46 @@ class Conversation extends StatelessWidget {
                     alignment: Alignment.topRight,
                   ),
                 ),
+              ),
+              Stack(
+                children: [
+                  Positioned(
+                    top: 100,
+                    left: 150,
+                    child: Image.asset(
+                      'assets/robot.png',
+                      alignment: Alignment.center,
+                    ),
+                  ),
+                ],
+              ),
+              Stack(
+                children: [
+                  Positioned(
+                    top: 310,
+                    left: 130,
+                    child: Text(
+                      'Hi Im GigglyGizmo',
+                      style: GoogleFonts.chivo(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w200,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    top: 335,
+                    left: 150,
+                    child: Text(
+                      'I may Laugh alot',
+                      style: GoogleFonts.chivo(
+                        color: const Color.fromARGB(255, 45, 235, 245),
+                        fontWeight: FontWeight.w200,
+                        fontSize: 15,
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
