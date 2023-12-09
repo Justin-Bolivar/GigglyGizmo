4f52cbdbfffa62b5f347a3ced78deaff8fb57b7d
Donnalyn <donnalynrrma@gmail.com>
2023-12-09 13:12:22 +0800

    added login form in login screen

--- lib/screens/log_in.dart
+++ lib/screens/log_in.dart
@@ -7,6 +7,54 @@ class LogIn extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      body: Stack(
+        children: [
+          Image(
+            fit: BoxFit.cover,
+            height: double.infinity,
+            width: double.infinity,
+            image: AssetImage('assets/Background.png')),
+          Align(
+                alignment: Alignment.center,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Image(
+                      height: 200,
+                      width: 200,
+                      image: AssetImage('assets/loginpage/AllWhite_Logo_w_text.png')),
+                    SizedBox(height: 30,),
+                    Padding(
+                      padding: EdgeInsets.all(20.0),
+                      child: TextField(
+                        decoration: InputDecoration(
+                        hintText: 'Enter Username',
+                        label: Text('Username'),
+                          fillColor: Color(0xffD8D8DD),
+                        filled: true,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(20.0),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Enter Password',
+                          label: Text('Password'),
+                          suffixIcon: Icon(Icons.visibility_off),
+                          fillColor: Color(0xffD8D8DD),
+                          filled: true,
+                        ),
+                      ),
+                    ),
+              
+                  ],
+                ),
+              )
+            ],
+          ),
+        );
   }
 }
