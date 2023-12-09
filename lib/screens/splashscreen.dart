b7d7f1004cfeec1f3edea54da5d8e319c162a2cc
Donnalyn <donnalynrrma@gmail.com>
2023-12-09 10:31:30 +0800

    added splashscreen (stateful widget)

---
+++ lib/screens/splashscreen.dart
@@ -0,0 +1,16 @@
+//screen before login
+import 'package:flutter/material.dart';
+
+class Splashscreen extends StatefulWidget {
+  const Splashscreen({Key? key}) : super(key: key);
+
+  @override
+  State<Splashscreen> createState() => SplashscreenState();
+}
+class SplashscreenState extends State<Splashscreen> {
+  
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
