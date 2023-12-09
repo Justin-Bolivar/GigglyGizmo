142310a07473b3ff71b4a95d25d53bc2fd4d4e1e
Donnalyn <donnalynrrma@gmail.com>
2023-12-09 11:28:51 +0800

    finished designing splashscreen

--- lib/screens/splashscreen.dart
+++ lib/screens/splashscreen.dart
@@ -8,9 +8,34 @@ class Splashscreen extends StatefulWidget {
   State<Splashscreen> createState() => SplashscreenState();
 }
 class SplashscreenState extends State<Splashscreen> {
-  
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Stack(
+        children: [
+          const Image(
+            fit: BoxFit.cover,
+            height: double.infinity,
+            width: double.infinity,
+            image: AssetImage('assets/Background.png')),
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [const Color(0xFF2D81FF).withOpacity(0.6),
+                        const Color(0xFF25D1A8).withOpacity(0.6)],
+                begin: Alignment.bottomCenter,
+                end: Alignment.topCenter)
+            ),
+          ),
+          const Align(
+            alignment: Alignment.center,
+            child: Image(
+              height: 250,
+              width: 250,
+              image: AssetImage('assets/loginpage/AllWhite_Logo_w_text.png')))
+        ],
+      ),
+    );
   }
 }
